101c3aa438224f5f7e179de98db27632b3cb009a
Benjamin Clewer <benjiclewer@gmail.com>
2021-10-01 09:19:42 +0930

    setting time and formatting

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,14 @@
 console.log("test");
+
+// setting time and format
+function update() {
+    var nowTime = moment();
+    var currentDay = $('#currentDay');
+    currentDay.text(nowTime.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+};
+
+// refreshing and rewriting time ever 1000ms/1sec
+$(document).ready(function() {
+    update();
+    setInterval(update, 1000);
+});
