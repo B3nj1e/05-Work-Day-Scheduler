2775b19f31716401fe0fe424ec9e1f17811ede02
Benjamin Clewer <benjiclewer@gmail.com>
2021-10-01 10:33:05 +0930

    time check between task and current time, bugs

--- Assets/script.js
+++ Assets/script.js
@@ -12,3 +12,22 @@ $(document).ready(function() {
     update();
     setInterval(update, 1000);
 });
+
+var currentTime = moment();
+console.log(currentTime.format("h:mm:ss"))
+
+function timeCheck() {
+    x = ["9:00:00", "10:00:00", "11:00:00", "12:00:00", "13:00:00", "14:00:00", "15:00:00", "16:00:00", "17:00:00"]
+    console.log(x);
+    for (i = 0; i < 9; i++) {
+    console.log(x[i]);
+    console.log("#".concat(x[i]));
+    var past = moment(x[i], "hh:mm:ss").isBefore(currentTime.format("hh:mm:ss"), "hh:mm:ss");
+    if (past) {
+        $("#".concat(x[i])).addClass('past');
+    } else {
+        $()
+    }
+}}
+
+timeCheck()
